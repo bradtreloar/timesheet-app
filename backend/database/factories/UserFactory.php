2d09c1853ecaf146ac28da6a2dfe42214341c6ac
Brad Treloar <brad.treloar@pm.me>
2023-05-16 22:37:19 +0930

    Hash password passed in attributes

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -14,6 +16,16 @@ class UserFactory extends Factory
      * @var string
      */
     protected $model = User::class;
+
+    public function create($attributes = [], ?Model $parent = null)
+    {
+        $password = $attributes['password'] ?? NULL;
+        if ($password !== NULL) {
+            $attributes['password'] = Hash::make($password);
+        }
+
+        return parent::create($attributes, $parent);
+    }
 
     /**
      * Define the model's default state.
@@ -27,7 +39,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'phone_number' => $this->faker->unique()->phoneNumber(),
             'accepts_reminders' => true,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
             'is_admin' => false,
             'default_values' => $this->faker->defaultShifts(),
